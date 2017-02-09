fb801acd9f24e365177bf24905f59dc74dd90125
happyer888 <keithly521@gmail.com>
2017-02-10 01:29:11 +0800

    Basic ToastUtils for android to show a short simple toast in any position.

---
+++ ToastUtils/ToastUtils.java
@@ -0,0 +1,19 @@
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Authorized by Keithly 2017
+ */
+
+public class ToastUtils {
+    private static Toast sToast;
+
+    public static void showToast(Context context, String msg) {
+        if (sToast == null) {
+            sToast = Toast.makeText(context.getApplicationContext(), msg, Toast.LENGTH_SHORT);
+        }
+        sToast.setText(msg);
+        sToast.show();
+    }
+}
