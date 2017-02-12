8e5d6763d6ac6325c0a06c7cb54da6521391f8fb
happyer888 <keithly521@gmail.com>
2017-02-12 23:28:07 +0800

    ActivityUtils added...

---
+++ ActivityUtils/ActivityUtils.java
@@ -0,0 +1,39 @@
+﻿
+
+import android.app.Activity;
+import java.util.ArrayList;
+
+/**
+ * 一键退出(BaseActivity+ActivityUtils)
+ */
+public class ActivityUtils {
+    // 声明一个集合用于记录所有打开的活动  
+    private static ArrayList<Activity> sList = new ArrayList<Activity>();
+
+    /**
+     * 加入活动对象--------->onCreate
+     * @param activity Activity
+     */
+    public static void add(Activity activity) {
+        sList.add(activity);
+    }
+
+    /**
+     * 移除活动对象--------->onDestroy
+     * @param activity Activity
+     */
+    public static void remove(Activity activity) {
+        sList.remove(activity);
+    }
+
+    /**
+     * 关闭所有的活动--------->close
+     */
+    public static void removeAll() {
+        for (Activity activity : sList) {
+            if (!activity.isFinishing()) {
+                activity.finish();
+            }
+        }
+    }
+}
