a167b963518b1162fabcf97b195d87838fff150f
happyer888 <keithly521@gmail.com>
2017-02-12 23:26:11 +0800

    ActivityUtils added and ToastUtils updated

--- ToastUtils/ToastUtils.java
+++ ToastUtils/ToastUtils.java
@@ -7,13 +7,26 @@ public class ToastUtils {
     private static Toast sToast;
 
     /**
-     * Shows a toast message while be called.
+     * Shows a short toast message while be called.
      * @param context Context
-     * @param msg  Message
+     * @param msg     Message
      */
     public static void showToast(Context context, String msg) {
         if (sToast == null) {
             sToast = Toast.makeText(context.getApplicationContext(), msg, Toast.LENGTH_SHORT);
+        }
+        sToast.setText(msg);
+        sToast.show();
+    }
+
+    /**
+     * Shows a long toast message while be called.
+     * @param context Context
+     * @param msg     Message
+     */
+    public static void showLongToast(Context context, String msg) {
+        if (sToast == null) {
+            sToast = Toast.makeText(context.getApplicationContext(), msg, Toast.LENGTH_LONG);
         }
         sToast.setText(msg);
         sToast.show();
